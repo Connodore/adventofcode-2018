d5e1258d61199be88e25131a144232c727af6837
Connor J Billings <cjbillin@millersville.edu>
2018-12-01 20:38:07 -0500

    Finished Day 1 - part 1

---
+++ day-1/part-1.cc
@@ -0,0 +1,32 @@
+/**
+ * Author: Connor Billings
+ * Date  : 1 December 2018
+ *
+ * Description:
+ *  Solution for Advent of Code: Day 1 - part 1
+ *
+ * Notes:
+ *  I learned that I could create iterators from streams
+ *  from: https://www.linuxtopia.org/online_books/programming_books/c++_practical_programming/c++_practical_programming_142.html
+ *
+ **/
+
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <numeric>
+
+using std::cin;
+using std::cout;
+
+int main(int argc, char *argv[])
+{
+    std::ifstream in("data/part-1.txt");
+
+    cout << std::accumulate(std::istream_iterator<long>(in),
+                            std::istream_iterator<long>(),
+                            0l);
+
+    return EXIT_SUCCESS;
+}
